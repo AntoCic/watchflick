ccacddeb47138ac26528c603e54c60488d251dc1
AntoCic <anto.cic.127@gmail.com>
2024-07-25 18:03:43 +0200

    FIX small bug from netlify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
   return (
     <div className='container mx-auto px-4 my-4 text-center'>
       {movies && movies.map((movie) => (
-        <div key={movie.id} className='border-2 border-slate-700 inline-block p-2 rounded m-3' >
+        <div key={movie.id} className='border-2 border-slate-700 inline-block p-2 rounded my-3 w-full' >
           <img src={'https://image.tmdb.org/t/p/w342' + movie.img_main} className='inline-block' alt="" />
           <p className='text-2xl'> {movie.title} </p>
           {movie.title !== movie.original_title && <p className='text-start'>Titolo Originale: {movie.original_title}</p>}
